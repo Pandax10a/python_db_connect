27685abed92849c82135d49825efbe50926b31af
panda1 <blueblue0011@gmail.com>
2022-09-24 15:57:49 -0700

    fixed with tries and except

--- app.py
+++ app.py
@@ -19,17 +19,27 @@ for each in result:
     print(each)
 
 def user_price():
-    the_num = input('what price you want? ')
-    only_one(the_num)
-    return the_num
+    while True:
+        try:
+            the_num = int(input('what price you want? '))
+            only_one(the_num)
+            return the_num
+        except ValueError:
+            print('use only number')
 
 def only_one(num):
     conn = mariadb.connect(user=dbcreds.user, password=dbcreds.password,
     host=dbcreds.host, port=dbcreds.port, database=dbcreds.database)
 
     cursor = conn.cursor()
-    
-    cursor.execute('CALL price_greater_than(?)', [num])
+    while True:
+        try:
+            cursor.execute('CALL price_greater_than(?)', [num])
+            break
+        except mariadb.OperationalError:
+            print('use an integer')
+            
+        
     result = cursor.fetchall()
     cursor.close()
     conn.close()
