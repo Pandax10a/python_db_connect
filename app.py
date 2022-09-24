01466ca386a05cd50da157c0155af1a9ce2dbfb7
panda1 <blueblue0011@gmail.com>
2022-09-24 15:41:01 -0700

    it works

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from sqlite3 import Cursor
+
 import dbcreds
 import mariadb
+
 
 conn = mariadb.connect(user=dbcreds.user, password=dbcreds.password,
 host=dbcreds.host, port=dbcreds.port, database=dbcreds.database)
@@ -9,4 +10,32 @@ cursor = conn.cursor()
 
 cursor.execute('CALL all_items')
 result = cursor.fetchall()
-print(result)
+
+
+cursor.close()
+conn.close()
+
+for each in result:
+    print(each)
+
+def user_price():
+    the_num = input('what price you want? ')
+    only_one(the_num)
+    return the_num
+
+def only_one(num):
+    conn = mariadb.connect(user=dbcreds.user, password=dbcreds.password,
+    host=dbcreds.host, port=dbcreds.port, database=dbcreds.database)
+
+    cursor = conn.cursor()
+    
+    cursor.execute('CALL price_greater_than(?)', [num])
+    result = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    print(result)
+
+# user_price(result)
+
+user_price()
+    
